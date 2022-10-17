11bcf4855b07b8cdf49d0ce183fd91ca4601da07
luoxingting <luoxingting@bytedance.com>
2022-10-18 01:06:03 +0800

    11

---
+++ bin/my-cli.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const create = require('../lib/create.ts');
+const program = require('commander');
+program.version(require('../package.json').version);
+program
+    .command('create <name>')
+    .description('create a new project')
+    .action(name => {
+        create(name).then(()=>{
+            process.exit(); // 创建成功，终止进程
+        }).catch(err => {
+            console.log(err);
+        });
+    })
+program.parse(process.argv);
---
+++ lib/create.ts
@@ -0,0 +1,15 @@
+const download = require('download-git-repo'); // 远程拉取模版文件
+const path = require('path');
+module.exports = name => {
+    return new Promise((resolve, reject) => {
+        const fullName = process.cwd() + "/" + name; // 获取文件路径
+        const url = 'https://github.com:aiyuekuang/react_home#master';
+        download(url, fullName, {clone: true}, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve('创建成功!');
+            }
+        })
+    })
+}
