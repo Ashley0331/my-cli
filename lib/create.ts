76db81dc7da5e3de0b24f572cebf4cd755d7fedb
luoxingting <luoxingting@bytedance.com>
2022-10-20 11:48:58 +0800

    添加选项，加载动画，判断目录是否存在

--- lib/create.ts
+++ lib/create.ts
@@ -1,56 +1,56 @@
-const inquirer = require('inquirer');
-const download = require('download-git-repo'); // 远程拉取模版文件
-const fs = require('fs');
-const chalk = require('chalk');
-const path = require('path');
-const ora = require('ora'); // 加载动画
+const inquirer = require("inquirer");
+const download = require("download-git-repo"); // 远程拉取模版文件
+const fs = require("fs");
+const chalk = require("chalk");
+const path = require("path");
+const ora = require("ora"); // 加载动画
 
 const template = {
-    'react': {
-        url: 'https://github.com:aiyuekuang/react_home#master'
-    },
-    "vue": {
-        url: 'https://github.com:aiyuekuang/vue_home#master'
-    }
-}
+  react: {
+    url: "https://github.com:aiyuekuang/react_home#master",
+  },
+  vue: {
+    url: "https://github.com:aiyuekuang/vue_home#master",
+  },
+};
 
 const config = [
-    {
-        type: 'list',
-        name: 'type',
-        message: 'Project Type',
-        choices: ['vue','react']
-    },
-    {
-        type:'input',
-        name:'name',
-        message:'Project Name',
-        default: 'cli-test'
-    }
-]
+  {
+    type: "list",
+    name: "type",
+    message: "Project Type",
+    choices: ["vue", "react"],
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Project Name",
+    default: "cli-test",
+  },
+];
 
 module.exports = () => {
-    const spinner = ora('正在拉取模版...');
-    inquirer.prompt(config).then(({ name, type }) => {
-        if (name.length <= 0) {
-            console.log('Project name is missing.');
-            process.exit();
-        }
-        spinner.start();
-        const url = template[type].url;
-        const fullName = process.cwd() + '/' + name;
-        if(fs.existsSync(fullName)){ // 判断目录是否已经存在
-            spinner.fail(chalk.red('目录已存在'));
-            process.exit();
-        }
-        download(url, fullName, { clone: true } ,(err) => {
-            if(err){
-                spinner.fail(chalk.red('模版创建失败'))
-                console.log(err)
-            }else{
-                spinner.succeed(chalk.green('模版创建成功'));
-            }
-        })
-
-    })
-}
+  const spinner = ora("正在拉取模版...");
+  inquirer.prompt(config).then(({ name, type }) => {
+    if (name.length <= 0) {
+      console.log("Project name is missing.");
+      process.exit();
+    }
+    spinner.start();
+    const url = template[type].url;
+    const fullName = process.cwd() + "/" + name;
+    if (fs.existsSync(fullName)) {
+      // 判断目录是否已经存在
+      spinner.fail(chalk.red("目录已存在"));
+      process.exit();
+    }
+    download(url, fullName, { clone: true }, (err) => {
+      if (err) {
+        spinner.fail(chalk.red("模版创建失败"));
+        console.log(err);
+      } else {
+        spinner.succeed(chalk.green("模版创建成功"));
+      }
+    });
+  });
+};
